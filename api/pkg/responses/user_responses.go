ef12f69815f08b9d3c1f23662f97233c5ac6c78e
ZeydAga <zeydaga5@gmail.com>
2025-02-15 03:08:34 +0300

    Delete api directory

--- api/pkg/responses/user_responses.go
+++
@@ -1,9 +0,0 @@
-package responses
-
-import "github.com/NdoleStudio/httpsms/pkg/entities"
-
-// UserResponse is the payload containing entities.User
-type UserResponse struct {
-	response
-	Data entities.User `json:"data"`
-}
